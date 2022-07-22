1ff0226d75ba84ae46e4a851f5c0826ba62b8151
CHETAN G <80114842+ChetanGk123@users.noreply.github.com>
2022-07-22 21:07:30 +0530

    updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { AdmissionFormComponent } from "./admission-form/admission-form.component";
 import { FeVoucherComponent } from "./feVoucher/feVoucher.component";
 import { TcComponent } from "./tc/tc.component";
 
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path:"feeVoucher",
     component:FeVoucherComponent
+  },
+  {
+    path:"admissionForm",
+    component:AdmissionFormComponent
   },
   {
     path:"tc",
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { FeVoucherComponent } from './feVoucher/feVoucher.component';
 import { AppRoutingModule } from './app-routing.module';
 import { TcComponent } from './tc/tc.component';
+import { AdmissionFormComponent } from './admission-form/admission-form.component';
 @NgModule({
   declarations: [
     AppComponent,
       FeVoucherComponent,
-      TcComponent
+      TcComponent,
+      AdmissionFormComponent
    ],
   imports: [
     AppRoutingModule,
