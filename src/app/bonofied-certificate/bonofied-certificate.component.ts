cbee4e2f9fc6ac08b4525b2d3c0c11ab9abae42a
CHETAN G <80114842+ChetanGk123@users.noreply.github.com>
2022-10-14 20:59:59 +0100

    updated

--- src/app/bonofied-certificate/bonofied-certificate.component.ts
+++ src/app/bonofied-certificate/bonofied-certificate.component.ts
@@ -21,24 +21,11 @@ export class BonofiedCertificateComponent implements OnInit {
   }
 
   async generatePDF(action = 'open') {
-    pdfMake.fonts = {
-      JosefinSans: {
-        normal: `${window.location.origin}/assets/JosefinSans-Regular.ttf`,
-        italic: `${window.location.origin}/assets/JosefinSans-Italic.ttf`,
-        bold: `${window.location.origin}/assets/JosefinSans-Bold.ttf`,
-      },
-      Kannada: {
-        normal: `${window.location.origin}/assets/NotoSansKannada/NotoSansKannada-Regular.ttf`,
-        italic: `${window.location.origin}/assets/NotoSansKannada/NotoSansKannada-Italic.ttf`,
-        bold: `${window.location.origin}/assets/NotoSansKannada/NotoSansKannada-Bold.ttf`,
-      },
-    };
     let docDefinition = {
       pageSize: 'A4',
 
       defaultStyle: {
         fontSize: 16,
-        font: 'JosefinSans',
       },
       pageMargins: [20, 0],
       content: [
@@ -56,62 +43,90 @@ export class BonofiedCertificateComponent implements OnInit {
                           columns: [
                             [
                               {
-                                text: 'Date:______________',
-                                margin: [30, 30, 0, 10],
-                              },
-                              {
                                 columns: [
                                   [
                                     {
                                       text: [
                                         {
                                           text: [
-                                            'BONOFIED CERTIFICATE\n',
-                                            {
-                                              text: '.......................................................',
-                                              fontSize: 15,
-                                            },
+                                            'BIRTH / CAST CERTIFICATE\n',
                                           ],
                                           characterSpacing: 2,
                                         },
                                       ],
                                       fontSize: 25,
                                     },
+                                    {
+                                      text: [
+                                        {
+                                          text: [
+
+                                            {
+                                              text: '...............................................................................',
+                                              fontSize: 15,
+
+                                            },
+                                          ],
+                                          characterSpacing: 2,
+                                        },
+                                      ],
+                                      margin: [0, -10, 0, 0],
+                                      fontSize: 25,
+                                    },
                                   ],
                                 ],
-                                margin: [0, 0, 0, -15],
+                                margin: [0, 10, 0, -15],
                                 alignment: 'center',
                               },
                               {
                                 text: [
-                                  'This is to certify that Mr./Mrs. ',
+                                  'Name of the School/College ',
+                                  {
+                                    text: 'Royal Palace International School, Lokapur.',
+                                    decoration: 'underline',
+                                  },
+                                  ' This is to certify that the Birth date of Shri/Kumari/Kumar. ',
                                   {
                                     text: 'Chetan G k',
                                     decoration: 'underline',
                                   },
-                                  ' S/O or D/O of Mr./Ms.',
+                                  ' as record in the school register  is ',
                                   {
-                                    text: 'Govindaraju',
+                                    text: '31-05-2014',
                                     decoration: 'underline',
                                   },
-                                  ' bearing roll number ',
+                                  ', (in words) ',
                                   {
-                                    text: '2BA15IS092',
+                                    text: 'Thirty One May Two Thousand Fourteen,',
                                     decoration: 'underline',
                                   },
-                                  ' is a student of ',
+                                  ' & cast is ',
                                   {
-                                    text: '2nd Standard,',
+                                    text: 'Hindu - Lingayat',
                                     decoration: 'underline',
                                   },
-                                  ' for the academic year ',
+                                  '\n Place of Birth: ',
                                   {
-                                    text: '2019 - 2020',
+                                    text: 'Bagalkot',
                                     decoration: 'underline',
                                   },
-                                  '. He/She is a bonified student of Royal Palace International School.',
+                                  '\t Tq: ',
+                                  {
+                                    text: 'Bagalkot',
+                                    decoration: 'underline',
+                                  },
+                                  '\t Diatrict: ',
+                                  {
+                                    text: 'Bagalkot',
+                                    decoration: 'underline',
+                                  },
+                                  '\n Admission No: ',
+                                  {
+                                    text: '19/2020-21',
+                                    decoration: 'underline',
+                                  },
                                 ],
-                                margin: [30, 30],
+                                margin: [20, 20],
                                 lineHeight: 1.5,
                                 alignment: 'justify',
                               },
@@ -119,20 +134,20 @@ export class BonofiedCertificateComponent implements OnInit {
                                 columns: [
                                   [
                                     {
-                                      text: '__________________________\n Royal Palace International School\n Lokapur.(Seal)',
+                                      text: '\n Place: Lokapur\n Date: 19-12-2021',
                                       fontSize: 12,
                                       alignment: 'center',
                                     },
                                   ],
                                   [
                                     {
-                                      text: '________________\n Signature\n Registrar/Principal/Dean',
+                                      text: '________________\n Signature\n Headmaster/Principal',
                                       fontSize: 12,
                                       alignment: 'center',
                                     },
                                   ],
                                 ],
-                                margin: [30, 10, 30, 30],
+                                margin: [10, 10, 10, 10],
                               },
                             ],
                           ],
@@ -144,88 +159,88 @@ export class BonofiedCertificateComponent implements OnInit {
               ],
             ],
           },
-          margin: [20, 40, 20, 30],
+          margin: [20, 35, 20, 20],
         },
-        {
-          table: {
-            widths: ['*'],
-            body: [
-              [
-                {
-                  columns: [
-                    [
-                      {
-                        text: 'Date:______________',
-                        margin: [30, 30, 0, 10],
-                      },
-                      {
-                        columns: [
-                          [
-                            {
-                              text: [
-                                {
-                                  text: [
-                                    'BONOFIED CERTIFICATE\n',
-                                    {
-                                      text: '.......................................................',
-                                      fontSize: 15,
-                                    },
-                                  ],
-                                  characterSpacing: 2,
-                                },
-                              ],
-                              fontSize: 25,
-                            },
-                          ],
-                        ],
-                        margin: [0, 0, 0, -15],
-                        alignment: 'center',
-                      },
-                      {
-                        text: [
-                          'This is to certify that Mr./Mrs. ',
-                          { text: 'Chetan G k', decoration: 'underline' },
-                          ' S/O or D/O of Mr./Ms.',
-                          { text: 'Govindaraju', decoration: 'underline' },
-                          ' bearing roll number ',
-                          { text: '2BA15IS092', decoration: 'underline' },
-                          ' is a student of ',
-                          { text: '2nd Standard,', decoration: 'underline' },
-                          ' for the academic year ',
-                          { text: '2019 - 2020', decoration: 'underline' },
-                          '. He/She is a bonified student of Royal Palace International School.',
-                        ],
-                        margin: [30, 30],
-                        lineHeight: 1.5,
-                        alignment: 'justify',
-                      },
-                      {
-                        columns: [
-                          [
-                            {
-                              text: '__________________________\n Royal Palace International School\n Lokapur.(Seal)',
-                              fontSize: 12,
-                              alignment: 'center',
-                            },
-                          ],
-                          [
-                            {
-                              text: '________________\n Signature\n Registrar/Principal/Dean',
-                              fontSize: 12,
-                              alignment: 'center',
-                            },
-                          ],
-                        ],
-                        margin: [30, 10, 30, 30],
-                      },
-                    ],
-                  ],
-                },
-              ],
-            ],
-          },
-          margin: [20, 40, 20, 30],
-        },
+        // {
+        //   table: {
+        //     widths: ['*'],
+        //     body: [
+        //       [
+        //         {
+        //           columns: [
+        //             [
+        //               {
+        //                 text: 'Date:______________',
+        //                 margin: [30, 30, 0, 10],
+        //               },
+        //               {
+        //                 columns: [
+        //                   [
+        //                     {
+        //                       text: [
+        //                         {
+        //                           text: [
+        //                             'BONOFIED CERTIFICATE\n',
+        //                             {
+        //                               text: '.......................................................',
+        //                               fontSize: 15,
+        //                             },
+        //                           ],
+        //                           characterSpacing: 2,
+        //                         },
+        //                       ],
+        //                       fontSize: 25,
+        //                     },
+        //                   ],
+        //                 ],
+        //                 margin: [0, 0, 0, -15],
+        //                 alignment: 'center',
+        //               },
+        //               {
+        //                 text: [
+        //                   'This is to certify that Mr./Mrs. ',
+        //                   { text: 'Chetan G k', decoration: 'underline' },
+        //                   ' S/O or D/O of Mr./Ms.',
+        //                   { text: 'Govindaraju', decoration: 'underline' },
+        //                   ' bearing roll number ',
+        //                   { text: '2BA15IS092', decoration: 'underline' },
+        //                   ' is a student of ',
+        //                   { text: '2nd Standard,', decoration: 'underline' },
+        //                   ' for the academic year ',
+        //                   { text: '2019 - 2020', decoration: 'underline' },
+        //                   '. He/She is a bonified student of Royal Palace International School.',
+        //                 ],
+        //                 margin: [30, 30],
+        //                 lineHeight: 1.5,
+        //                 alignment: 'justify',
+        //               },
+        //               {
+        //                 columns: [
+        //                   [
+        //                     {
+        //                       text: '__________________________\n Royal Palace International School\n Lokapur.(Seal)',
+        //                       fontSize: 12,
+        //                       alignment: 'center',
+        //                     },
+        //                   ],
+        //                   [
+        //                     {
+        //                       text: '________________\n Signature\n Registrar/Principal/Dean',
+        //                       fontSize: 12,
+        //                       alignment: 'center',
+        //                     },
+        //                   ],
+        //                 ],
+        //                 margin: [30, 10, 30, 30],
+        //               },
+        //             ],
+        //           ],
+        //         },
+        //       ],
+        //     ],
+        //   },
+        //   margin: [20, 40, 20, 30],
+        // },
       ],
     };
     const pdfDocGenerator = pdfMake.createPdf(docDefinition);
